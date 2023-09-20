3d7e3697c7dd43259f4e301e0761b5307d56dd98
faliq1405 <noer.faliq@gmail.com>
2023-09-20 18:51:07 +0700

    first commit

---
+++ plugins/info-creator.js
@@ -0,0 +1,16 @@
+function handler(m) {
+  
+  const kontak = {
+	"displayName": 'My owner',
+	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN: VynaaChan\nitem1.TEL;waid=6282389924037:6282389924037\nitem1.X-ABLabel:\nJangan Spam Kak\nURL;Email Owner:vynaachan@gmail.com\nORG: INI OWNER\nEND:VCARD`
+}
+
+conn.sendMessage(m.chat, { contacts: { contacts: [kontak] }}, { quoted: m })
+  
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+
+handler.command = /^(owner|creator)$/i
+
+export default handler
